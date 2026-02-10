dcd8e725d41f8bef638fc7ee4c8fa417465c0ab7
rajat <rajatsingla@192.168.1.3>
2026-02-10 12:11:33 +0530

    add root route for render health check

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const isPrime = (n) => {
 
 const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
 const lcm = (a, b) => (a * b) / gcd(a, b);
+// ROOT route (for Render check)
+app.get("/", (req, res) => {
+  res.send("BFHL API is running");
+});
 
 // GET /health
 app.get("/health", (req, res) => {
